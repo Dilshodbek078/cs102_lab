191f7765a4df5cc2393b76df290c3c69dab5c38a
Dilshodbek078 <d.orazbayev@newuu.uz>
2025-09-25 09:58:29 +0500

    Create problem2.cpp

---
+++ problem2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x;
+    cin >> x;
+    cout << x%2<< endl;
+    if (x%2==0)
+        cout <<"Even"<< endl;
+    else
+        cout <<"Odd"<< endl;
+    return 0;
+}
